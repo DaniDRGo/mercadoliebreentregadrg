64e3bd2638f9c91a3aea57b4165ec6630abd9d27
Danidrg <danielnddrago@gmail.com>
2022-05-31 15:42:33 -0300

    cuarta carga

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require ('path')
-const port = 3000 process.env.PORT
+const port = process.env.PORT || 3000
 app.use(express.static('public'))
 app.listen(port,()=>{
     console.log("servidor levantado")
