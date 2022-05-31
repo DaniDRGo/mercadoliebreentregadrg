d22a8f137301081631824fa710e5cde1dfe01069
Danidrg <danielnddrago@gmail.com>
2022-05-31 00:31:52 -0300

    segunda carga

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require ('path')
-const port = process.env.PORT
+const port = 3000 /*process.env.PORT*/
 app.use(express.static('public'))
 app.listen(port,()=>{
     console.log("servidor levantado")
@@ -9,3 +9,9 @@ app.listen(port,()=>{
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'views/home.html'))
 })
+app.get('/register',(req,res)=>{
+    res.sendFile(__dirname + '/views/register.html')
+})
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
